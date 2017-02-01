7868e85ad4a50ff06e38630f903873c2c81fd3ce
Timothy Meixell <misternu@gmail.com>
2017-01-31 21:39:49 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  // This is the main component, state lives here
+  constructor(props) {
+    super(props)
+
+    this.setState({
+
+    });
+  }
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default App;
